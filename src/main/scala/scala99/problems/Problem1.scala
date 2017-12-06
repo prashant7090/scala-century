fb03c534ada286d3cfabc96ff54f9a6f210d05be
Prashant Sable <prashant7090@gmail.com>
2017-12-06 11:56:01 +0530

    Scala sbt seed project with test suite.

---
+++ src/main/scala/scala99/problems/Problem1.scala
@@ -0,0 +1,8 @@
+package scala99.problems
+
+/**
+  * Created by prashant on 6/12/17.
+  */
+object Problem1 {
+
+}
---
+++ src/test/scala/scala99/problems/Problem1Suite.scala
@@ -0,0 +1,16 @@
+package scala99.problems
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+/**
+  * Created by prashant on 6/12/17.
+  */
+@RunWith(classOf[JUnitRunner])
+class Problem1Suite extends FunSuite{
+  test("Test"){
+    assert(1===1)
+  }
+
+}
