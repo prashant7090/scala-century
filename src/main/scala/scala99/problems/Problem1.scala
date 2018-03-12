8621798cce82d9abf030f683bc2dc6113cbb9f2f
Prashant Sable <Prashant7090@gmail.com>
2018-03-12 19:44:18 +0530

    Solution for problem 1.

--- src/main/scala/scala99/problems/Problem1.scala
+++ src/main/scala/scala99/problems/Problem1.scala
@@ -5,4 +5,17 @@ package scala99.problems
   */
 object Problem1 {
 
+  def findLastElementOfList(list: List[Int]): Int ={
+        list match {
+          case Nil => throw new NoSuchElementException("empty list")
+          case head::Nil => head;
+          case head:: tail => findLastElementOfList(tail)
+        }
+  }
+
+  def main(args: Array[String]): Unit = {
+      println("Last Element: " + findLastElementOfList(List(1,2,3,4,5,6)));
+  }
+
+
 }
--- src/test/scala/scala99/problems/Problem1Suite.scala
+++ src/test/scala/scala99/problems/Problem1Suite.scala
@@ -9,8 +9,16 @@ import org.scalatest.FunSuite
   */
 @RunWith(classOf[JUnitRunner])
 class Problem1Suite extends FunSuite{
-  test("Test"){
-    assert(1===1)
+  import Problem1.findLastElementOfList
+
+  test("test last element of non empty list"){
+    assert(findLastElementOfList(List(1,2,3,4)) === 4)
+  }
+
+  test("test empty list which should throw exception"){
+    assertThrows[NoSuchElementException]{
+      findLastElementOfList(List())
+    }
   }
 
 }
