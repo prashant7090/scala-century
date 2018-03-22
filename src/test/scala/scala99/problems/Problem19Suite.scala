cfa66775fd4e2cfae3eb31c6d66c194d4b0ad779
System Administrator <Prashant7090@gmail.com>
2018-03-22 19:42:00 +0530

    Added test cases for problem 19.

---
+++ src/test/scala/scala99/problems/Problem19Suite.scala
@@ -0,0 +1,28 @@
+package scala99.problems
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+/**
+  * Created by prashant on 22/03/2018.
+  */
+
+@RunWith(classOf[JUnitRunner])
+class Problem19Suite extends FunSuite{
+  import Problem19.rotate;
+
+  test("Should rotate the list by 3 element left"){
+    assert( rotate(3,List('a','b','c', 'd', 'e', 'f', 'g','h','i','j','k')) == List('d', 'e', 'f', 'g','h','i','j','k,'a','b','c' ) )
+  }
+
+  test("Should rotate the list by 2 element right"){
+    assert( rotate(2,List('a','b','c', 'd', 'e', 'f', 'g','h','i','j','k')) == List('k','j','a','b','c', 'd', 'e', 'f', 'g','h','i') )
+  }
+
+  test("List should remaining same for 0 rotate operation"){
+    assert( rotate(0,List('a','b','c', 'd', 'e', 'f', 'g','h','i','j','k')) == List('a','b','c', 'd', 'e', 'f', 'g','h','i','j','k') )
+  }
+
+
+}
